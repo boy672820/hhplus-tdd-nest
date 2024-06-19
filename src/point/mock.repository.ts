9f2d2f7d352c84cd33c0e53c7b699436d43cb9dd
Seonzoo <boy672820@gmail.com>
2024-06-19 23:32:19 +0900

    PointRepositoryMock -> MockRepository 변경

--- src/point/mock.repository.ts
+++ src/point/mock.repository.ts
@@ -4,7 +4,7 @@ import { Repository } from './repository.interface';
 /**
  * Mock for Repository class
  */
-export class PointRepositoryMock<TModel extends UserPoint | PointHistory>
+export class MockRepository<TModel extends UserPoint | PointHistory>
   implements Repository<TModel>
 {
   findById = jest.fn();
